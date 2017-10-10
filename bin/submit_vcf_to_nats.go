fc3a1224c0bd53745626abd7df5def66ef21da9e
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-10-10 16:39:05 -0400

    changed import statement to reflect name of gitlab repo, changed default values for -s and -f and perform a validation on passed arguments, changed name of method to reflected suggested changes by golint

--- bin/submit_vcf_to_nats.go
+++ bin/submit_vcf_to_nats.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
-	"github.com/rdeborja/vcfgoutils"
+	"gitlab.com/uhn/vcfgoutils"
 )
 
 func usage() {
-	log.Fatalf("Usage: submit_vcf_to_nats [-s nats://<server>:<port>] [-f /path/to/vcf/file]")
+	fmt.Println("Usage: submit_vcf_to_nats [-s nats://<server>:<port>] [-f /path/to/vcf/file]")
+	flag.PrintDefaults()
 }
 
 func main() {
 	// setup the command line arguments
-	urlPtr := flag.String("s", "nats://localhost:4222", "The NATS server URLs (separated by comma) (default: nats://localhost:4222)")
+	urlPtr := flag.String("s", "", "The NATS server URLs (separated by comma) (required)")
 	vcfPtr := flag.String("f", "", "The VCF file to transmit (required)")
 	log.SetFlags(0)
 	flag.Usage = usage
 	flag.Parse()
-	vcfgoutils.SendVcfToNatsAsJson(urlPtr, vcfPtr)
+	if *urlPtr == "" {
+		fmt.Println("Missing argument -s")
+		flag.Usage()
+	} else if *vcfPtr == "" {
+		fmt.Println("Missing argument -f")
+		flag.Usage()
+	} else {
+		vcfgoutils.SendVcfToNatsAsJSON(urlPtr, vcfPtr)
+	}
 }
