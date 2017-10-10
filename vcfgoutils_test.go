a629a6a7c2ac64cc3bc6c82a6c299810d230cdf8
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-10-10 17:08:55 -0400

    adding testing code

--- vcfgoutils_test.go
+++ vcfgoutils_test.go
@@ -1,1 +1,29 @@
 package vcfgoutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/brentp/vcfgo"
+	"gitlab.com/uhn/vcfgoutils"
+)
+
+func TestConvertVcfToJSON(t *testing.T) {
+	vcfFile := "examples/vcfgoutils_test.vcf"
+	f, _ := os.Open(vcfFile)
+	rdr, err := vcfgo.NewReader(f, false)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		variant := rdr.Read()
+		if variant == nil {
+			break
+		}
+		simpleVariant := vcfgoutils.ConvertVcfToJSON(variant)
+		b, _ := json.Marshal(simpleVariant)
+		fmt.Println(b)
+	}
+}
